c1a63b705c09991683ebed4a675d2fe608e75bf1
danigelabert <73782173+danigelabert@users.noreply.github.com>
2023-11-06 13:59:52 +0100

    Initial commit

---
+++ app/src/main/java/com/danigelabert/leccion5_recycleview_v2/data/Datasource.kt
@@ -0,0 +1,22 @@
+package com.danigelabert.leccion5_recycleview_v2.data
+
+import com.danigelabert.leccion5_recycleview_v2.R
+import com.danigelabert.leccion5_recycleview_v2.model.Affirmation
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/com/danigelabert/leccion5_recycleview_v2/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.danigelabert.leccion5_recycleview_v2.model
+
+data class Affirmation(val stringResourceId: Int)
